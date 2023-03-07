e84fda9313437ab0b382f77048c8b545f4fb0b2d
CaitlinKirby <caitlin.kirby@bbc.co.uk>
2023-03-07 10:46:33 +0000

    Initial Commit

---
+++ MiniSoundsChallenge/MiniSoundsChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniSoundsChallengeApp.swift
+//  MiniSoundsChallenge
+//
+//  Created by Caitlin Kirby on 07/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct MiniSoundsChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
